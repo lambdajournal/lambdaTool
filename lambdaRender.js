7f72660da802219bc56163abb20d3a465b98d1ab
aguarino <andrea.guarino.1989@gmail.com>
2015-03-21 21:06:50 +0100

    first commit

---
+++ lambdaRender.js
@@ -0,0 +1,87 @@
+var colorProvider = function*() {
+  while (true) {
+    yield "black";
+    yield "gray";
+    yield "navy";
+    yield "blue";
+    yield "maroon";
+    yield "purple";
+    yield "fuchsia";
+    yield "red";
+    yield "orange";
+  }
+}
+
+var fontSizeProvider = function*(initialSize, deltaSize) {
+  var currentSize = initialSize;
+  while (true) {
+    yield currentSize;
+    if (currentSize - deltaSize >= 8)
+      currentSize -= deltaSize;
+  }
+}
+
+var bracketsProvider = function*() {
+  while (true) {
+    yield { open: "{", close: "}"};
+    yield { open: "[", close: "]"};
+    yield { open: "(", close: ")"};    
+  }
+}
+
+var formatterProvider = function*(initialSize, deltaSize) {
+  var colorProviderInstance = colorProvider();
+  var fontSizeProviderInstance = fontSizeProvider(initialSize, deltaSize);
+  var bracketsProviderInstance = bracketsProvider();
+  var level = yield {};
+  var formats = [];
+  while (true) {
+    if (level < formats.length) {
+      yield formats[level];
+      level = yield {};
+      continue;
+    }
+      
+    formats.push({ 
+      color: colorProviderInstance.next().value,
+      fontSize: fontSizeProviderInstance.next().value,
+      brackets: bracketsProviderInstance.next().value
+    });
+  }
+}
+
+var lambdaRender = function (lambdaTerm, initialSize, deltaSize) {
+  initialSize = initialSize || 50;
+  deltaSize = deltaSize || 5;
+  var node = lambdaParser.parse(lambdaTerm);
+  var formatterProviderInstance = formatterProvider(initialSize, deltaSize);
+  return visit(node, formatterProviderInstance, 0);
+};
+
+var visit = function (node, formatterProviderInstance, level) {
+  formatterProviderInstance.next(level).value;
+  var currentFormat = formatterProviderInstance.next(level).value;
+  var nodeContent;
+  
+  if(typeof(node) === "string") {
+    nodeContent = node;
+  }
+  else if(node.type === "expr") {
+    nodeContent = "λ";
+    nodeContent += node.vars.map(function (v) {
+      return visit(v, formatterProviderInstance, level);
+    }).join(',');
+    nodeContent += ".";
+    nodeContent += visit(node.body, formatterProviderInstance, level);
+  }
+  else if(node.type === "app") {
+    var firstContent = visit(node.first, formatterProviderInstance, level + 1);
+    firstContent = typeof(node.first) === "Object" ? ("(" + firstContent + ")") : firstContent;
+    var secondContent = visit(node.second, formatterProviderInstance, level + 1);
+    secondContent = typeof(node.second) === "Object" ? ("(" + secondContent + ")") : secondContent;
+    var subContent = "<sub style=\"font-size: 50%;\">" + level + "</sub>";
+    nodeContent = currentFormat.brackets.open + subContent + firstContent + " " + secondContent + subContent + currentFormat.brackets.close;
+  }
+
+  return "<span style=\"font-size: " + currentFormat.fontSize + "; color: "+  currentFormat.color+ "; \">" + nodeContent + "</span>";
+};
